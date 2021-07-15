c1b5bb43cb0761da60f35a339cb8fe036140d53a
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-07-16 01:08:37 +0200

    Update Add property liked

--- src/model/Cocktail.ts
+++ src/model/Cocktail.ts
@@ -2,15 +2,18 @@ export interface Icocktail {
   name: string;
   image: any;
   ingredients: string[];
+  liked: boolean;
 }
 
 export class Cocktail implements Icocktail {
   name;
   image;
   ingredients;
-  constructor(name: string, image: any, ingredients: string[]) {
+  liked;
+  constructor(name: string, image: any, ingredients: string[], liked: boolean) {
     this.name = name;
     this.image = image;
     this.ingredients = ingredients;
+    this.liked = liked;
   }
 }
