328c2dcc4ae7af23483a39a741a8343265729f31
Benjamin Desole <69907169+BenGex31@users.noreply.github.com>
2021-07-17 14:27:15 +0200

    Update Add icon checkbox on AppBar component

--- src/Components/AppBar/AppBar.tsx
+++ src/Components/AppBar/AppBar.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./AppBar.css";
+import Checkbox from "@material-ui/core/Checkbox";
+import Favorite from "@material-ui/icons/Favorite";
+import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
 
-export function AppBar(props) {
+interface IAppBar {
+  filter: Function;
+}
+
+export function AppBar(props: IAppBar) {
   return (
     <div className="AppBar">
       <h1>Cocktail Bar</h1>
+      <Checkbox
+        //style={{ position: "absolute", right: "10px" }}
+        icon={<FavoriteBorder />}
+        checkedIcon={<Favorite />}
+        name="checkedH"
+        //checked={props.cocktail.liked}
+      />
       <input
         placeholder="Search..."
         onChange={(event) => {
